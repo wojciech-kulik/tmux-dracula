d1f8871f19220d2043e6b243b48a4039821df5b3
Wojciech Kulik <3128467+wojciech-kulik@users.noreply.github.com>
2023-12-01 15:01:06 +0100

    Update dracula.sh

--- scripts/dracula.sh
+++ scripts/dracula.sh
@@ -273,7 +273,7 @@ main()
 
   # Window option
   if $show_powerline; then
-    tmux set-window-option -g window-status-current-format "#[fg=${gray},bg=${dark_purple}]${left_sep}#[fg=${white},bg=${purple}] #I #W${current_flags} #[fg=${purple},bg=${gray}]${left_sep}"
+    tmux set-window-option -g window-status-current-format "#[fg=${gray},bg=${dark_purple}]${left_sep}#[fg=${white},bg=${light_purple}] #I #W${current_flags} #[fg=${light_purple},bg=${gray}]${left_sep}"
   else
     tmux set-window-option -g window-status-current-format "#[fg=${white},bg=${dark_purple}] #I #W${current_flags} "
   fi
